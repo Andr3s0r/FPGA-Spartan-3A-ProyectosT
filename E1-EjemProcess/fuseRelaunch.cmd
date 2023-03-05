07d218ef276ab6fd9a8b634cee851905dcc18cc6
Andr3s0r <123838826+Andr3s0r@users.noreply.github.com>
2023-03-04 21:48:54 -0600

    Delete E1-EjemProcess directory

--- E1-EjemProcess/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -o "C:/Users/AndresOrtiz/Desktop/Ejemplos Tesis/E1-EjemProcess/main_isim_beh.exe" -prj "C:/Users/AndresOrtiz/Desktop/Ejemplos Tesis/E1-EjemProcess/main_beh.prj" "work.main" 
--- E1-EjemProcess/readme.txt
+++
@@ -1,2 +0,0 @@
-Este es el programa que contiene el Ejemplo 1 del uso de process. Para utilizarlo descarga la carpeta completa y utiliza ISE Design Suite 14.7 para poder
-abrir el proyecto.
